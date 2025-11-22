a09913c1ba44869fb54563fdeb817b88463c2c45
Warren Alphonso <complaints@warr.in>
2025-11-21 23:16:35 -0800

    switch std to 0.02, remove slow python shape code

--- nanogpt/src/nanogpt/model.py
+++ nanogpt/src/nanogpt/model.py
@@ -6,6 +6,7 @@ import torch
 import urllib3
 from jaxtyping import Float, Int
 from torch import Tensor, nn
+from torch.nn import functional as F
 
 
 @dataclasses.dataclass
@@ -30,21 +31,19 @@ SmallConfig = Config(
     mlp_hidden_dim= 3072
 )
 
-class LinearKaiming(nn.Linear):
-    def __init__(self, in_features:int, out_features:int, *, bias=True, scale=None):
+class Linear(nn.Linear):
+    def __init__(self, in_features: int, out_features: int, *, bias=True, scale=None):
         super().__init__(in_features, out_features, bias=bias)
-        std = math.sqrt(2.0 / in_features)
+        std = 0.02
         if scale: std *= scale
-        with torch.no_grad():
-            self.weight.normal_(0.0, std)
-            if bias:
-                self.bias.zero_()
+        torch.nn.init.normal_(self.weight, std=std)
+        if bias:
+            torch.nn.init.zeros_(self.bias)
 
-class EmbeddingKaiming(nn.Embedding):
-    def __init__(self, vocab_size:int, embed_size:int):
+class Embedding(nn.Embedding):
+    def __init__(self, vocab_size: int, embed_size: int):
         super().__init__(vocab_size, embed_size)
-        with torch.no_grad():
-            self.weight = torch.nn.init.kaiming_normal_(self.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        torch.nn.init.normal_(self.weight, std=0.02)
 
 class Block(nn.Module):
     def __init__(self, config: Config):
@@ -53,13 +52,13 @@ class Block(nn.Module):
         residual_scaling_factor = 1.0 / math.sqrt(2 * config.n_layers)
         self.ln_1 = nn.LayerNorm(config.d_model)
         self.attn = nn.ModuleDict({
-            "c_attn": LinearKaiming(config.d_model, 3 * config.d_model), # 3 different linear projections (Q, K, V)
-            "c_proj": LinearKaiming(config.d_model, config.d_model, scale=residual_scaling_factor),
+            "c_attn": Linear(config.d_model, 3 * config.d_model), # 3 different linear projections (Q, K, V)
+            "c_proj": Linear(config.d_model, config.d_model, scale=residual_scaling_factor),
         })
         self.ln_2 = nn.LayerNorm(config.d_model)
         self.mlp = nn.ModuleDict({
-            "c_fc": LinearKaiming(config.d_model, config.mlp_hidden_dim),
-            "c_proj": LinearKaiming(config.mlp_hidden_dim, config.d_model, scale=residual_scaling_factor),
+            "c_fc": Linear(config.d_model, config.mlp_hidden_dim),
+            "c_proj": Linear(config.mlp_hidden_dim, config.d_model, scale=residual_scaling_factor),
         })
 
         self.register_buffer("causal_mask", torch.tril(torch.ones((config.sequence_length, config.sequence_length))).view(1, 1, config.sequence_length, config.sequence_length))
@@ -69,21 +68,15 @@ class Block(nn.Module):
         qkv = self.attn["c_attn"](self.ln_1(x))
         q, k, v = qkv.split(C, dim=2)
 
-        def to_heads(m: Float[Tensor, "B T C"]) -> Float[Tensor, "B N T (C//N)"]:
-            return m.view(B, T, self.config.n_heads, self.config.d_head).permute(0, 2, 1, 3)
-
-        q, k, v = map(to_heads, (q, k, v))
-        att = (q @ k.transpose(-2, -1)) / math.sqrt(self.config.d_head)
-        assert isinstance(att, Float[Tensor, "B N T T"])
-        att = att.masked_fill(self.causal_mask[:, :, :T, :T] == 0, float('-inf')).softmax(dim=-1)
-
-        y = att @ v
-        assert isinstance(y, Float[Tensor, "B N T (C//N)"])
+        k = k.view(B, T, self.config.n_heads, C // self.config.n_heads).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.config.n_heads, C // self.config.n_heads).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.config.n_heads, C // self.config.n_heads).transpose(1, 2) # (B, nh, T, hs)
+        y = F.scaled_dot_product_attention(q, k, v, is_causal=True)
         y = y.transpose(1, 2).contiguous().view(B, T, C)
 
         x = x + self.attn["c_proj"](y)
 
-        x = x + self.mlp["c_proj"](nn.functional.gelu(self.mlp["c_fc"](self.ln_2(x))))
+        x = x + self.mlp["c_proj"](F.gelu(self.mlp["c_fc"](self.ln_2(x))))
 
         return x
 
@@ -91,16 +84,16 @@ class GPT2(nn.Module):
     def __init__(self, config: Config):
         super().__init__()
         self.config = config
-        self.wte = EmbeddingKaiming(config.vocab_size, config.d_model)
-        self.wpe = EmbeddingKaiming(config.sequence_length, config.d_model)
+        self.wte = Embedding(config.vocab_size, config.d_model)
+        self.wpe = Embedding(config.sequence_length, config.d_model)
         self.h = nn.ModuleList([Block(config) for _ in range(config.n_layers)])
         self.ln_f = nn.LayerNorm(config.d_model)
-        self.lm_head = LinearKaiming(config.d_model, config.vocab_size, bias=False)
+        self.lm_head = Linear(config.d_model, config.vocab_size, bias=False)
         self.lm_head.weight = self.wte.weight
 
     def forward(self, tokens: Int[Tensor, "B T"]) -> Float[Tensor, "B T V"]:
         B, T = tokens.shape
-        x = self.wte(tokens) + self.wpe(torch.arange(T, dtype=torch.long, device=tokens.device))
+        x = self.wte(tokens) + self.wpe(torch.arange(0, T, dtype=torch.long, device=tokens.device))
 
         for block in self.h:
             x = block(x)
@@ -128,30 +121,33 @@ def dataloader(B, T):
 
 if __name__ == "__main__":
     if torch.cuda.is_available():
-        print("Using CUDA")
         device = "cuda"
     elif torch.backends.mps.is_built():
-        print("Using MPS")
         device = "mps"
     else:
-        print("Using CPU")
         device = "cpu"
 
+    print(f"Using {device=}")
     torch.set_default_device(device)
+    torch.set_float32_matmul_precision("high")
 
     model = GPT2(SmallConfig)
     model.to(device)
     model = torch.compile(model)
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
-    for i, (x, y) in enumerate(dataloader(8, 1024)):
-        t0 = time.time()
-        x, y = x.to(device), y.to(device)
-        optimizer.zero_grad()
-        logits = model(x)
-        loss = nn.functional.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1))
-        loss.backward()
-        optimizer.step()
+    for epoch in range(10):
+        print(f"Epoch {epoch+1}/10")
+        for i, (x, y) in enumerate(dataloader(16, 1024)):
+            t0 = time.time()
+            x, y = x.to(device), y.to(device)
+            optimizer.zero_grad()
+            with torch.autocast(device_type=device, dtype=torch.bfloat16):
+                logits = model(x)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), y.view(-1))
+            loss.backward()
+            optimizer.step()
+            torch.cuda.synchronize() if device == "cuda" else None
 
-        t1 = time.time()
-        print(f"({(t1-t0)*1000:.1f}ms) {i=}, {loss.item()=}")
+            t1 = time.time()
+            print(f"({(t1-t0)*1000:.1f}ms) {i=}, {loss.item()=}")
